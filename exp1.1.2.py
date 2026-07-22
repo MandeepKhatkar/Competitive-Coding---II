9b06cef42288a5eee4ee8dce05651193e87b94ab
MandeepKhatkar <khatkarmandeep2007@gmail.com>
2026-07-22 14:47:45 +0530

    Refactor productExceptSelf functions and update calls

--- exp1.1.2.py
+++ exp1.1.2.py
@@ -1,77 +1,45 @@
-def productExceptSelf_bruteforce(nums):
-
+def bruteforce(nums):
     n = len(nums)
     answer = []
-
     for i in range(n):
-
         product = 1
-
         for j in range(n):
-
             if i != j:
                 product *= nums[j]
-
         answer.append(product)
-
     return answer
 
 
-def productExceptSelf_prefix_suffix(nums):
-
+def prefix_suffix(nums):
     n = len(nums)
-
     prefix = [1] * n
     suffix = [1] * n
     answer = [1] * n
-
-    
     for i in range(1, n):
         prefix[i] = prefix[i - 1] * nums[i - 1]
-
-    
     for i in range(n - 2, -1, -1):
         suffix[i] = suffix[i + 1] * nums[i + 1]
-
-    
     for i in range(n):
         answer[i] = prefix[i] * suffix[i]
-
     return answer
 
 
-def productExceptSelf_optimized(nums):
-
+def optimized(nums):
     n = len(nums)
-
-    answer = [1] * n
-
-    
+    answer = [1] * n    
     for i in range(1, n):
-        answer[i] = answer[i - 1] * nums[i - 1]
-
-    
+        answer[i] = answer[i - 1] * nums[i - 1]  
     right = 1
-
     for i in range(n - 1, -1, -1):
         answer[i] *= right
         right *= nums[i]
-
     return answer
 
 
 nums = list(map(int, input("Enter array elements separated by spaces: ").split()))
-
-print("\nResults")
-print("-----------------------------------------------")
-
-result1 = productExceptSelf_bruteforce(nums)
-print("Approach 1 (Brute Force)           :", result1)
-
-result2 = productExceptSelf_prefix_suffix(nums)
-print("Approach 2 (Prefix-Suffix Arrays)  :", result2)
-
-result3 = productExceptSelf_optimized(nums)
-print("Approach 3 (Optimized)             :", result3)
-
-print("-----------------------------------------------")
+r1 = bruteforce(nums)
+print("Brute Force:", r1)
+r2 = prefix_suffix(nums)
+print("Prefix-Suffix Arrays:", r2)
+r3 = optimized(nums)
+print("Optimized:", r3)
